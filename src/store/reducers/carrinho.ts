99df805e806832452da036a90b9748ceb2fb9fa3
F3lipeB <152784982+F3lipeB@users.noreply.github.com>
2024-11-05 16:29:57 -0300

    commit do exercício

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+  favoritos: number[]
+}
+
+const initialState: CarrinhoState = {
+  itens: [],
+  favoritos: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
